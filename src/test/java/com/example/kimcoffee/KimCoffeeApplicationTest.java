645cee5f7499940126ed96200483e8853bc718c1
unnokid <kimki0609@gmail.com>
2022-05-06 21:17:51 +0900

    Add Test

---
+++ src/test/java/com/example/kimcoffee/KimCoffeeApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.kimcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimCoffeeApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ src/test/java/com/example/kimcoffee/repository/ProductJdbcRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.example.kimcoffee.repository;
+
+import com.example.kimcoffee.model.Category;
+import com.example.kimcoffee.model.Product;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.UUID;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.v5_7_latest;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ProductJdbcRepositoryTest {
+
+    static EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    static void setup() {
+        var config = aMysqldConfig(v5_7_latest)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = anEmbeddedMysql(config)
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+    }
+
+    @AfterAll
+    static void cleanup() {
+        embeddedMysql.stop();
+    }
+
+    @Autowired
+    ProductRepository productRepository;
+
+    private final Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
+    @Test
+    @Order(1)
+    @DisplayName("상품을 추가할 수 있음")
+    void 상품추가() {
+        productRepository.insert(newProduct);
+        List<Product> list = productRepository.findAll();
+        assertThat(list.isEmpty(), is(false));
+    }
+}
