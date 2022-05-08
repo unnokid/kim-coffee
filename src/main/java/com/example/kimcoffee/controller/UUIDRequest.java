66eba739a730d5650f92f7c735ada1b26730d88b
unnokid <kimki0609@gmail.com>
2022-05-08 21:04:19 +0900

    Update UUIDRequest

--- src/main/java/com/example/kimcoffee/controller/UUIDRequest.java
+++ src/main/java/com/example/kimcoffee/controller/UUIDRequest.java
@@ -3,7 +3,13 @@ package com.example.kimcoffee.controller;
 import java.util.UUID;
 
 public class UUIDRequest {
+    private final UUID id;
 
+    public UUIDRequest(UUID id) {
+        this.id = id;
+    }
 
-
+    public UUID getId() {
+        return id;
+    }
 }
